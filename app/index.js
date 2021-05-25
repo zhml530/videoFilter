f06d5081a3123da195605d2e32d726c5222748a4
Minliang Zhou <zhouminliang@microsoft.com>
2021-05-25 14:53:58 +0800

    update url

--- app/index.js
+++ app/index.js
@@ -1,4 +1,4 @@
-microsoftTeams.initialize(() => {}, ["https://localhost:9000", "https://lubobill1990.github.io"]);
+microsoftTeams.initialize(() => {}, ["https://localhost:9000", "https://zhml530.github.io"]);
 
 // This is the effect for processing
 let appliedEffect = {
